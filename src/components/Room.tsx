42d53d6fccb3e57e9080b1aacadffb1a230966a7
Rendrikson de Oliveira Soares <rendrikson.soares@codevasf.gov.br>
2022-03-14 17:30:49 -0300

    assistido até 1:01

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+
 type Props = {
 
 }
 
 const Room = (props: Props)=>{
+    let rooms: any[] = [];
+    axios.get('http://localhost:3000/rooms')
+    .then(response => {
+        rooms = response.data;
+    });
+    console.log(rooms);
     return(
         <div>
             <h1>Salas</h1>
             <ul>
                 <li>Sala 1</li>
+                <li>Sala 2</li>
             </ul>
         </div>
     )
