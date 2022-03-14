55888356127aa08ab1bada9a05143538429faaae
Rendrikson de Oliveira Soares <rendrikson.soares@codevasf.gov.br>
2022-03-14 17:14:34 -0300

    First commit

---
+++ src/components/Room.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+type Props = {
+
+}
+
+const Room = (props: Props)=>{
+    return(
+        <div>
+            <h1>Salas</h1>
+            <ul>
+                <li>Sala 1</li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Room;
